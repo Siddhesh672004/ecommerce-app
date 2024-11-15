772dc906b2c84d8e5dfc748f2eb26424b5f156b2
Siddhesh Chaudhari <siddheshavic67@gmail.com>
2024-11-15 16:05:29 +0530

    Home Changes

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -6,21 +6,21 @@ const Slider = () => {
     <>
         <Carousel className="m-4">
         <Carousel.Item interval={2000} className={styles.carouselContainer} >
-          <img src="https://drive.google.com/drive/u/1/folders/11P2G3Y3Knwjnbw1Y9ylyOOUi5c-9_AQF" alt="First slide"  className={styles.carouselImage}/>
+          <img src="https://cdn.pixabay.com/photo/2021/11/22/20/20/online-6817350_1280.jpg" alt="First slide"  className={styles.carouselImage}/>
           <Carousel.Caption>
             <h3>First slide label</h3>
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item interval={2000} className={styles.carouselContainer} >
-          <img src="https://drive.google.com/drive/u/1/folders/11P2G3Y3Knwjnbw1Y9ylyOOUi5c-9_AQF" alt="Second slide" className={styles.carouselImage}/>
+          <img src="https://media.istockphoto.com/id/822518448/photo/shopping-cart-icon-with-lines-pointing-on-different-places-around-the-globe-online-shopping.jpg?s=1024x1024&w=is&k=20&c=-Jghg_mBm2h1-76qrEzhOrDHwvajCUuHJxWjt-TsHtk=" alt="Second slide" className={styles.carouselImage}/>
           <Carousel.Caption>
             <h3>Second slide label</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item interval={2000} className={styles.carouselContainer} >
-          <img src="https://drive.google.com/drive/u/1/folders/11P2G3Y3Knwjnbw1Y9ylyOOUi5c-9_AQF" alt="Third slide" className={styles.carouselImage}/>
+          <img src="https://media.istockphoto.com/id/1304849847/photo/online-shopping-business-icon-application.jpg?s=1024x1024&w=is&k=20&c=ejo5PsVeH8xWEyypqee_8rACEdbWpraW1u0i913sUms=" alt="Third slide" className={styles.carouselImage}/>
           <Carousel.Caption>
             <h3>Third slide label</h3>
             <p>
